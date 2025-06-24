7cda31e143a76ae1b4a9e0d94372c2c93f827bf9
angkumar <angkumar787@gmail.com>
2025-06-24 14:09:30 -0400

    Initial Commit

---
+++ RecipeFinder/RecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderApp.swift
+//  RecipeFinder
+//
+//  Created by Angad Kumar on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeFinderTests/RecipeFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderTests.swift
+//  RecipeFinderTests
+//
+//  Created by Angad Kumar on 6/24/25.
+//
+
+import Testing
+@testable import RecipeFinder
+
+struct RecipeFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
